07c125a8df4b3a7b388d5d778cf4dd1eea2f4015
Kirti <kirtiagarwal119@gmail.com>
2025-07-30 03:05:49 +0530

    tablet support fixed

--- app/lib/apiService.js
+++ app/lib/apiService.js
@@ -2,7 +2,7 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const API_BASE_URL =
-  "https://memory-una-standing-define.trycloudflare.com/api"; // Local development server
+  "https://api.birdearner.com/api"; // Local development server
 
 // upload image categories are mentioned over here, above uploadImage function and in backend at /upload route
 /** @type {const} */
