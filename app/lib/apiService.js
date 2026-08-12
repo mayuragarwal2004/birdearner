96f96a6d7ff0422c4be7dd8115574e1bb12d2b9d
Dhanshree <dhanshreeshinde2003@gmail.com>
2026-08-12 13:22:14 +0530

    amount negotiation history maintain

--- app/lib/apiService.js
+++ app/lib/apiService.js
@@ -2,7 +2,7 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { Platform } from "react-native";
 
-const DEV_API_BASE_URL = "https://revision-professor-allied-trivia.trycloudflare.com/api";
+const DEV_API_BASE_URL = "https://generous-sharing-mirror-dts.trycloudflare.com/api";
 // const DEV_API_BASE_URL = "https://api.birdearner.com/api";
 
 const PROD_API_BASE_URL = "https://api.birdearner.com/api";
