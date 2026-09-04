c350ba1291152c341556fae51d3bbc5546c6c422
Dhanshree <dhanshreeshinde2003@gmail.com>
2026-09-04 11:45:14 +0530

    feat: allow own-job visibility in listings

--- app/lib/apiService.js
+++ app/lib/apiService.js
@@ -3,7 +3,7 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { Platform } from "react-native";
 
-const DEV_API_BASE_URL = "https://cheese-commented-photograph-harvard.trycloudflare.com/api";
+const DEV_API_BASE_URL = "https://royalty-agree-adam-jefferson.trycloudflare.com/api";
 // const DEV_API_BASE_URL = "https://api.birdearner.com/api";
 
 const PROD_API_BASE_URL = "https://api.birdearner.com/api";
