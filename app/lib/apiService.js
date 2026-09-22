ccdc725ce50dbecdedfcfb2c4955df1e077ebd3f
Dhanshree <dhanshreeshinde2003@gmail.com>
2026-09-22 19:09:33 +0530

    chore(app): update DEV_API_BASE_URL Cloudflare tunnel endpoint

--- app/lib/apiService.js
+++ app/lib/apiService.js
@@ -3,7 +3,7 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { Platform } from "react-native";
 
-const DEV_API_BASE_URL = "https://lights-bryan-athens-prince.trycloudflare.com/api";
+const DEV_API_BASE_URL = "https://vanilla-quantities-downtown-understood.trycloudflare.com/api";
 // const DEV_API_BASE_URL = "https://api.birdearner.com/api";
 
 const PROD_API_BASE_URL = "https://api.birdearner.com/api";
